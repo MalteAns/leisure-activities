a24dff37160bd345a0811e24f8116c0810ce7d6d
Malte <gathok1787@gmail.com>
2025-10-16 11:29:53 -0700

    iOS Build working.

--- composeApp/src/iosMain/kotlin/de/malteans/sosactivities/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/de/malteans/sosactivities/MainViewController.kt
@@ -2,5 +2,12 @@ package de.malteans.sosactivities
 
 import androidx.compose.ui.window.ComposeUIViewController
 import de.malteans.sosactivities.app.App
+import de.malteans.sosactivities.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initKoin()
+    }
+) {
+    App()
+}
